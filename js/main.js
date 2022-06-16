cc594eec872972922c081e2677eecb33f8ace719
Carlo1891 <carlo.18@live.it>
2022-06-16 16:12:06 +0200

    Impostazione intervallo FuzzBuzz

--- js/main.js
+++ js/main.js
@@ -6,3 +6,23 @@
 // Consigli del giorno:
 // Scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
 // Proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
+
+for (var n=1; n < 101; n++);
+// Numero internvallo 1 e 100
+{
+    // divisibili per 3 e 5 (3 * 5)
+    if (n % 15 == 0)
+        console.log("FizzBuzz");
+        // divisibile per 3
+    else if (n % 3 == 0)
+        console.log("Fizz");
+        // divisibile per 5
+    else if (n % 5 == 0)
+        console.log("Buzz");
+    else
+        console.log(n);
+}
+
+// BONUS 1: Crea un container nel DOM e aggiungendo (attravero la funzione append()) un elemento html con il numero o la stringa corretta da mostrare.
+
+// BONUS 2: Applica stili differenti agli elementi aggiunti al DOM nel BONUS 1, a seconda che il valore inserito sia un numero, un fizz, un buzz o un fizzbuzz.
