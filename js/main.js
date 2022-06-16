a70f498a8e8996ade09166635751d94761a83121
Carlo1891 <carlo.18@live.it>
2022-06-16 15:47:08 +0200

    Inizio

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// Scrivi un programma che stampi in console i numeri da 1 a 100,
+// ma che per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi “Buzz”.
+// Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
+// Prima di partire a scrivere codice poniamoci qualche domanda:
+// Come faccio a sapere se un numero è divisibile per un altro? Abbiamo visto qualcosa di particolare che possiamo usare?
+// Consigli del giorno:
+// Scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
+// Proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
